5c86d0ea5b356939674fe1dbd0f674e804835668
dongliang1986 <dongliang1986@gmail.com>
2012-08-15 01:19:40 +0800

    未完

--- server.py
+++ server.py
@@ -1,13 +1,28 @@
 import socket
-address = ('127.0.0.1', 31500)
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(address)
+sockTable={}
+def receiveHandler(data, addr):
+    handlerDict={'KEEPALIVE': receiveKLHandler}
+    lines = data.split('\n')
+    handlerDict[lines[0]](lines[1:], addr)
+def receiveKLHandler(lines, addr):
+    line = lines[0]
+    type,value = tuple(line.split())
+    if type != 'name':
+        sys.stderr.write('invalid option')
+        return
+    sockTable[addr] = value
+    pass
+if __name__ == '__main__':
+    address = ('127.0.0.1', 31500)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(address)
 
-while True:
-    data, addr = s.recvfrom(2048)
-    if not data:
-        print "client has exist"
-        break
-    print "received:", data, "from", addr
-
-s.close()
+        
+    while True:
+        data, addr = s.recvfrom(2048)
+        if not data:
+            print "client has exist"
+            break
+        print "received:", data, "from", addr
+        receiveHandler(data, addr)
+    s.close()
