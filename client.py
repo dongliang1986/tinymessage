3e68bfe5179bb1f2377c781a949eb5e8714e4ff0
dongliang1986 <dongliang1986@gmail.com>
2012-08-17 00:50:54 +0800

    增加LoginInfo类 receiveMSGHandler sendMSG函数

--- client.py
+++ client.py
@@ -3,7 +3,9 @@ import socket
 address = ('127.0.0.1', 31500)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-msg='KEEPALIVE\nname dongliang\n'
+msg='KEEPALIVE\ndongliang\n'
+s.sendto(msg, address)
+msg='MESSAGE\ndongliang\nhello\nworld\n'
 s.sendto(msg, address)
 
 s.close()
--- server.py
+++ server.py
@@ -1,17 +1,37 @@
-import socket
-sockTable={}
+﻿import socket
+class LoginInfo:
+    '''用户登录信息
+'''
+    def __init__(self):
+        self.sockTable={} #地址<->用户名
+        self.userTable={} #用户名<->地址
+    def insert(self, addr, user):
+        if self.sockTable.has_key(addr):
+            self.userTable.pop(self.sockTable[addr])
+        self.sockTable[addr] = user
+        self.userTable[user] = addr
+loginInfo = LoginInfo()
 def receiveHandler(data, addr):
-    handlerDict={'KEEPALIVE': receiveKLHandler}
-    lines = data.split('\n')
-    handlerDict[lines[0]](lines[1:], addr)
-def receiveKLHandler(lines, addr):
-    line = lines[0]
-    type,value = tuple(line.split())
-    if type != 'name':
-        sys.stderr.write('invalid option')
-        return
-    sockTable[addr] = value
-    pass
+    '''收包处理
+'''
+    handlerDict={'KEEPALIVE': receiveKLHandler,
+                 'MESSAGE' : receiveMSGHandler}
+    lineGen = (i for i in data.split('\n') if len(i) != 0)
+    line = lineGen.next()
+    handlerDict[line](lineGen, addr)
+    
+def receiveKLHandler(lineGen, addr):
+    line = lineGen.next()
+    loginInfo.insert(addr, line)
+def receiveMSGHandler(lineGen, addr):
+    receiver = lineGen.next()
+    sendMSG(receiver, lineGen)
+def sendMSG(receiver, lineGen):
+    message = [ i for i in lineGen]
+    message = '\n'.join(message)
+    
+    print receiver, message
+s = None  
 if __name__ == '__main__':
     address = ('127.0.0.1', 31500)
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
